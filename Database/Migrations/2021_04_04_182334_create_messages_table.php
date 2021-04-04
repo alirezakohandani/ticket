ff9b27bdfd861a9c9173e6ce20e33a0cd459e5c7
yasna <alireza.ko73@gmail.com>
2021-04-04 18:30:18 +0430

    Create messages migration

---
+++ Database/Migrations/2021_04_04_182334_create_messages_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use App\Http\Abstracts\ModularMigration;
+
+class CreateMessagesTable extends ModularMigration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedBigInteger('ticket_id');
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+
+            $table->unsignedBigInteger('person_id');
+            $table->foreign('person_id')->references('id')->on('persons')->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+            $this->additionalMigrations($table);
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+
+}
