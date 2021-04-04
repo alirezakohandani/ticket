a50c011ab6bb2d44cedfc1124fa1d2f3aa5b5bce
yasna <alireza.ko73@gmail.com>
2021-04-04 18:35:19 +0430

    Create ticket and message models

---
+++ Entities/Message.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Ticketing\Entities;
+
+use App\Http\Abstracts\ModularModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends ModularModel
+{
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'description'];
+
+    /**
+     * Get the ticket that owns the message.
+     *
+     * @return void
+     */
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class)
+    }
+
+
+    /**
+     * get the main meta fields of the table.
+     *
+     * @return array
+     */
+    public function mainMetaFields()
+    {
+        return [
+            //TODO: Fill this with the names of your meta fields, or remove the method if you do not want meta fields at all.
+        ];
+    }
+}
---
+++ Entities/Ticket.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Ticketing\Entities;
+
+use App\Http\Abstracts\ModularModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends ModularModel
+{
+    use SoftDeletes;
+
+
+    protected $fillable = ['user_id', 'type', 'status'];
+
+    /**
+     * Get the messages for the ticket.
+     *
+     * @return  void
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * get the main meta fields of the table.
+     *
+     * @return array
+     */
+    public function mainMetaFields()
+    {
+        return [
+            //TODO: Fill this with the names of your meta fields, or remove the method if you do not want meta fields at all.
+        ];
+    }
+}
