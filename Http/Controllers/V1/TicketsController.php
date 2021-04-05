f9337eda501ad380d111492b07acfb2fd03d95b5
yasna <alireza.ko73@gmail.com>
2021-04-05 19:04:19 +0430

    Create store method

--- Http/Controllers/V1/TicketsController.php
+++ Http/Controllers/V1/TicketsController.php
@@ -3,13 +3,35 @@
 namespace Modules\Ticketing\Http\Controllers\V1;
 
 use App\Http\Abstracts\ModularController;
+use Modules\Ticketing\Http\Requests\V1\TicketSaveRequest;
+use Modules\Ticketing\Services\Ticket\Front\Ticket;
 use Symfony\Component\HttpFoundation\Response;
 
 class TicketsController extends ModularController
 {
+    /**
+     * ticket variable
+     *
+     * @var [type]
+     */
+    private $ticket;
 
-    public function store()
+    /**
+     * Create a new event instance of ticket.
+     *
+     * @param Ticket $ticket
+     */
+    public function __construct(Ticket $ticket)
     {
+        $this->ticket = $ticket;
+    }
 
+    /**
+     *
+     *
+     */
+    public function store(TicketSaveRequest $request)
+    {
+        $this->ticket->store($request);
     }
 }
