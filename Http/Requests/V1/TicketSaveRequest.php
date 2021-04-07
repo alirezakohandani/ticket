79d7da8520d1333499277b97f9bf5298ba648c72
yasna <alireza.ko73@gmail.com>
2021-04-07 10:10:59 +0430

    Validation rules

---
+++ Http/Requests/V1/TicketSaveRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Modules\Ticketing\Http\Requests\V1;
+
+use App\Http\Abstracts\ModularFormRequest;
+use App\Models\Ticket;
+
+/**
+ * @property Ticket $model
+ */
+class TicketSaveRequest extends ModularFormRequest
+{
+    /** @var string */
+    protected $model_name = "ticket";
+
+    /** @var bool */
+    protected $should_allow_create_mode = true;
+
+
+
+    /**
+     * get the main validation rules.
+     *
+     * @return array
+     */
+    public function mainRules()
+    {
+        return [
+            'email'       => "required|email",
+            'type'        => "required|string",
+            'title'       => "required|max:255'",
+            'description' => "required|string",
+        ];
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function fillableFields()
+    {
+        return [
+            'type',
+            'email',
+            'title',
+            'description',
+        ];
+    }
+
+}
