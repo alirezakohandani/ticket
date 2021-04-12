2489e4fe55a3893450bce30fa1233d61d4db4f38
yasna <alireza.ko73@gmail.com>
2021-04-12 16:26:39 +0430

    Create index method

--- Http/Controllers/V1/Admin/TicketsController.php
+++ Http/Controllers/V1/Admin/TicketsController.php
@@ -4,9 +4,34 @@ namespace Modules\Ticketing\Http\Controllers\V1\Admin;
 
 
 use App\Http\Abstracts\ModularController;
+use App\Models\Ticket;
 
 
 class TicketsController extends ModularController
 {
 
+    /**
+     * Show tickets and messages for managers who have access to reply and close tickets
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $tickets = Ticket::all();
+        $result  = [];
+        foreach ($tickets as $ticket) {
+            $result[] = [
+                'id'       => hashid($ticket->id),
+                'status'   => $ticket->status,
+                //'title'    => $ticket->messages()->first()->title,
+                'messages' => $ticket->messages->map(function ($message) {
+                    return [
+                        'id'          => hashid($message->id),
+                        'description' => $message->description,
+                    ];
+                }),
+            ];
+        }
+        return $this->success([$result]);
+    }
 }
