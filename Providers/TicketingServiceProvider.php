dc5371165d4d1237491d4b4a41a37925eb86cfb8
yasna <alireza.ko73@gmail.com>
2021-04-12 17:53:53 +0430

    Register endpoint in service-provider

--- Providers/TicketingServiceProvider.php
+++ Providers/TicketingServiceProvider.php
@@ -4,6 +4,7 @@ namespace Modules\Ticketing\Providers;
 
 use App\Http\Abstracts\ModularProvider;
 use Modules\Ticketing\Entities\Traits\TicketingRelatedWithPersonTrait;
+use Modules\Ticketing\Http\Endpoints\V1\AdminTicketCloseEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\AdminTicketsShowEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\ShowTicketsUserEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\TicketSaveEndpoint;
@@ -35,6 +36,7 @@ class TicketingServiceProvider extends ModularProvider
         endpoint()->register(TicketShowEndpoint::class);
         endpoint()->register(ShowTicketsUserEndpoint::class);
         endpoint()->register(AdminTicketsShowEndpoint::class);
+        endpoint()->register(AdminTicketCloseEndpoint::class);
     }
 
 
