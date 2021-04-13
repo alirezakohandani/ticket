8cc40082e8ab9280738878df76940c9f276b652e
yasna <alireza.ko73@gmail.com>
2021-04-13 10:16:32 +0430

    Create and register an endpoint ticket change status

---
+++ Http/Endpoints/V1/AdminStatusChangeEndpoint.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Modules\Ticketing\Http\Endpoints\V1;
+
+use Modules\Endpoint\Services\EndpointAbstract;
+use Modules\Ticketing\Http\Controllers\V1\TicketsController;
+
+/**
+ * @api               {POST}
+ *                    /api/modular/v1/ticketing-admin-status-change
+ * @apiDescription    Administrators with reply access can change the status of the ticket
+ * @apiVersion        1.0.0
+ * @apiName           Change status
+ * @apiGroup          Ticketing
+ * @apiPermission     Admin
+ * @apiParam {int}    ref_number    ticket tracking code
+ * @apiParam {string} status        includes: ['pending', 'answerd', 'finished']
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *      "status": 200,
+ *      "metadata": {
+ *          "id": "hA5h1d"
+ *      },
+ *       "results": {
+ *          "ref_number": 21646355
+ *          "status": "answerd"
+ *          "developerMessage": trans(ticketing::developer.closed)
+ *          "userMessage": trans(ticketing::user.closed)
+ *      }
+ * }
+ * @apiErrorExample
+ * HTTP/1.1 400 Bad Request
+ * {
+ *      "status": 400,
+ *      "developerMessage": "endpoint::developerMessages.endpoint-403",
+ *      "userMessage": "Forbidden!",
+ *      "errorCode": "endpoint-403",
+ *      "moreInfo": "endpoint.moreInfo.endpoint-403",
+ *      "errors": []
+ * }
+ * @method Admin\TicketsController controller()
+ */
+class AdminStatusChangeEndpoint extends EndpointAbstract
+{
+    /**
+     * @inheritdoc
+     */
+    public static function getTitle(): string
+    {
+        return "Administrators with reply access can change the status of the ticket";
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function hasPermit(): bool
+    {
+        return user()->exists && user()->can("tickets.reply");
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getValidMethod(): string
+    {
+        return static::HTTP_POST;
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getControllerNamespace(): string
+    {
+        return 'Modules\Ticketing\Http\Controllers\V1\Admin';
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getController(): string
+    {
+        return 'TicketsController@test';
+    }
+}
+
--- Providers/TicketingServiceProvider.php
+++ Providers/TicketingServiceProvider.php
@@ -4,6 +4,7 @@ namespace Modules\Ticketing\Providers;
 
 use App\Http\Abstracts\ModularProvider;
 use Modules\Ticketing\Entities\Traits\TicketingRelatedWithPersonTrait;
+use Modules\Ticketing\Http\Endpoints\V1\AdminStatusChangeEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\AdminTicketCloseEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\AdminTicketsShowEndpoint;
 use Modules\Ticketing\Http\Endpoints\V1\ShowTicketsUserEndpoint;
@@ -37,6 +38,7 @@ class TicketingServiceProvider extends ModularProvider
         endpoint()->register(ShowTicketsUserEndpoint::class);
         endpoint()->register(AdminTicketsShowEndpoint::class);
         endpoint()->register(AdminTicketCloseEndpoint::class);
+        endpoint()->register(AdminStatusChangeEndpoint::class);
     }
 
 
