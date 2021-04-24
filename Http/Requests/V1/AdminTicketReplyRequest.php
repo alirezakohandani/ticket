dd0a3c8f801179f3749427bb93b8c159fe3a0541
alirezakohandani <alireza.ko73@gmail.com>
2021-04-24 09:58:16 +0430

    Create a request to respond to the ticket

---
+++ Http/Requests/V1/AdminTicketReplyRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Modules\Ticketing\Http\Requests\V1;
+
+use App\Http\Abstracts\ModularFormRequest;
+use App\Models\Ticket;
+
+/**
+ * @property Ticket $model
+ */
+class AdminTicketReplyRequest extends ModularFormRequest
+{
+    /** @var string */
+    protected $model_name = "Ticket";
+
+    /** @var bool */
+    protected $should_load_model = true;
+
+    /** @var bool */
+    protected $should_load_model_with_slug = true;
+
+    /** @var bool */
+    protected $should_allow_create_mode = false;
+
+    /** @var string */
+    protected $model_slug_attribute = "ref_number";
+
+    /** @var string */
+    protected $model_slug_column = "ref_number";
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function authorize()
+    {
+        return $this->model->canEdit();
+    }
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            'ref_number'  => "required|int",
+            'description' => "required|string",
+        ];
+    }
+
+
+}
