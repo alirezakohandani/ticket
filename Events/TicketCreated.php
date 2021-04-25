c8803d73adf0c10436b2f86e61fd0a5cd76a82d2
yasna <alireza.ko73@gmail.com>
2021-04-25 19:16:40 +0430

    Update TicketCreated event

--- Events/TicketCreated.php
+++ Events/TicketCreated.php
@@ -3,10 +3,29 @@
 namespace Modules\Ticketing\Events;
 
 use App\Http\Abstracts\ModularEvent;
+use App\Models\Ticket;
+use App\Models\User;
 
 class TicketCreated extends ModularEvent
 {
-    /** @var string */
-    public $model_name = "Ticket";
 
+    /** @var Ticket */
+    public $ticket;
+
+    /** @var User */
+    public $user;
+
+
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Ticket $ticket
+     * @param User   $user
+     */
+    public function __construct(Ticket $ticket, User $user)
+    {
+        $this->ticket = $ticket;
+        $this->user   = $user;
+    }
 }
