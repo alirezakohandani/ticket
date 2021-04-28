0009cba03f2a12118d3ac6e846b4004b3429186b
yasna <alireza.ko73@gmail.com>
2021-04-28 14:30:11 +0430

    Add title for notification

---
+++ Resources/lang/fa/notifications.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return [
+    "title"                 => "تیکت جدید ثبت شد",
+    "send_email_to_manager" => [
+        "title" => "ثبت تیکت جدید",
+    ],
+    "notification"          => [
+        "email" => "سلام تیکت به شماره :ref_number ثبت شده است.",
+
+    ],
+];
