14ce8f47fcae2f7d20ea7537d16dc70ea49f92ec
yasna <alireza.ko73@gmail.com>
2021-05-01 11:56:57 +0430

    Update tickets controller

--- Http/Controllers/V1/TicketsController.php
+++ Http/Controllers/V1/TicketsController.php
@@ -2,12 +2,11 @@
 
 namespace Modules\Ticketing\Http\Controllers\V1;
 
+use App\Classes\SimpleListRequest;
 use App\Classes\TrackingNumber;
 use App\Http\Abstracts\ModularController;
 use App\Models\Message;
 use App\Models\Person;
-use App\Models\User;
-use App\Models\Ticket;
 use Modules\Ticketing\Events\TicketCreated;
 use Modules\Ticketing\Http\Requests\V1\TicketSaveRequest;
 use Modules\Ticketing\Http\Requests\V1\TicketShowRequest;
@@ -27,13 +26,27 @@ class TicketsController extends ModularController
 
             $person->registerUser($request->email);
         }
+
         $person_id = $person->getPersonId($request->email);
-        $ticket    = $request->model
-             ->createTicket($request, $person_id, $this->generateRefNumber());
-        Message::instance()->setMessage($request, $ticket);
+
+        $ticket = $request->model->batchSave([
+            'person_id'  => $person_id,
+            'ref_number' => TrackingNumber::id2No(rand(1, 999999)),
+            'type'       => $request->type,
+            'status'     => 'pending',
+
+        ], ['email']);
+
+        Message::instance()->batchSave([
+            'ticket_id'   => $ticket->id,
+            'person_id'   => 2,
+            'title'       => $request->title,
+            'description' => $request->description,
+        ])
+        ;;
         event(new TicketCreated($ticket));
         return $this->success([
-             'ref_number' => $ticket->ref_number,
+            'ref_number' => $ticket->ref_number,
         ]);
 
 
@@ -52,15 +65,15 @@ class TicketsController extends ModularController
     {
 
         return $this->success([
-             'id'       => hashid($request->model->id),
-             'status'   => $request->model->status,
-             'title'    => $request->model->messages()->first()->title,
-             'messages' => $request->model->messages->map(function ($message) {
-                 return [
-                      'id'          => hashid($message->id),
-                      'description' => $message->description,
-                 ];
-             }),
+            'id'       => hashid($request->model->id),
+            'status'   => $request->model->status,
+            'title'    => $request->model->messages()->first()->title,
+            'messages' => $request->model->messages->map(function ($message) {
+                return [
+                    'id'          => hashid($message->id),
+                    'description' => $message->description,
+                ];
+            }),
         ]);
 
     }
@@ -72,42 +85,23 @@ class TicketsController extends ModularController
      *
      * @return Response
      */
-    public function showTicketsUser()
+    public function showTicketsUser(SimpleListRequest $request)
     {
         $result = [];
         foreach (\user()->tickets as $ticket) {
             $result[] = [
-                 'id'       => hashid($ticket->id),
-                 'status'   => $ticket->status,
-                 'title'    => $ticket->messages()->first()->title,
-                 'messages' => $ticket->messages->map(function ($message) {
-                     return [
-                          'id'          => hashid($message->id),
-                          'description' => $message->description,
-                     ];
-                 }),
+                'id'       => hashid($ticket->id),
+                'status'   => $ticket->status,
+                'title'    => $ticket->messages()->first()->title,
+                'messages' => $ticket->messages->map(function ($message) {
+                    return [
+                        'id'          => hashid($message->id),
+                        'description' => $message->description,
+                    ];
+                }),
             ];
         }
         return $this->success([$result]);
-    }
-
-
-
-    /**
-     * Generates ref_number which is not in the table
-     *
-     * @return int
-     */
-    protected function generateRefNumber()
-    {
-        //TODO we don't need ref_number in table, the tracking number generates based on id
-        do {
-
-            $ref_number = TrackingNumber::id2No(rand(1, 999999));
-
-        } while (Ticket::where('ref_number', $ref_number)->exists());
-
-        return $ref_number;
     }
 
 
